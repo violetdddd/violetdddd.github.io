c8a9e3406fec2c9ef0ff364fee6f3e8b7b07dcc8
Junduo Zheng <1253314323@qq.com>
2023-07-18 11:37:51 +0800

    TodoList
    test

---
+++ vue-project/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="pt-1 flex flex-row gap-2">
+        <h3 class="w-full p-2 rounded-lg border border-gray-400 text-xl text-gray-900 bg-yellow-50">{{ msg }}</h3>
+        <button @click="$emit('delete')" class="flex-1 px-4 py-2 rounded-lg font-semibold text-md text-white bg-green-600 hover:bg-green-700 focus:ring-2 focus:ring-green-300">Done</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+    msg:{
+        type:String,
+        required:true,
+    }
+})
+defineEmits({
+    delete:null,
+})
+</script>
---
+++ vue-project/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
